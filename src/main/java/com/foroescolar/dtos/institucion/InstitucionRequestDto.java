6a3339a930e2c43996a2b087e1424102fd8fc7ea
Govil-web <gvillegas1276@hotmail.com>
2025-07-16 18:44:38 -0500

    se retiran las validaciones de institucion

--- src/main/java/com/foroescolar/dtos/institucion/InstitucionRequestDto.java
+++ src/main/java/com/foroescolar/dtos/institucion/InstitucionRequestDto.java
@@ -1,22 +1,22 @@
 package com.foroescolar.dtos.institucion;
 
-import jakarta.validation.constraints.NotBlank;
-
 import java.io.Serializable;
 
 public record InstitucionRequestDto(
 
-        @NotBlank(message = "El nombre de la institucion no puede estar vacio")
+        //@NotBlank(message = "El nombre de la institucion no puede estar vacio")
         String nombre,
-        @NotBlank(message = "La direccion de la institucion no puede estar vacia")
+        //@NotBlank(message = "La direccion de la institucion no puede estar vacia")
         String direccion,
-        @NotBlank(message = "El telefono de la institucion no puede estar vacio")
+        //@NotBlank(message = "El telefono de la institucion no puede estar vacio")
         String telefono,
-        @NotBlank(message = "El email de la institucion no puede estar vacio")
+        //@NotBlank(message = "El email de la institucion no puede estar vacio")
         String email,
-        @NotBlank(message = "La identificacion de la institucion no puede estar vacia")
+        //@NotBlank(message = "La identificacion de la institucion no puede estar vacia")
         String identificacion,
-        @NotBlank(message = " Seleccione que tipo de institucion es")
+        //@NotBlank(message = " Seleccione que tipo de institucion es")
+
+
         String nivelEducativo
 
 
